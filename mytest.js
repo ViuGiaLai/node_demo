58690fe6f92dc89d4ba720ca6a4299c38532d13a
ViuRemix <viu106018@donga.edu.vn>
2025-09-13 16:58:39 +0700

    add fuction tinhtuoi

--- mytest.js
+++ mytest.js
@@ -2,3 +2,11 @@ function showMyName(name,age) {
   console.log(`Tên: ${name}, Tuổi: ${age}`);
 }
 showMyName("Rmah Viu ", 18)
+
+function tinhtuoi(namsinh) {
+  const namhientai = new Date().getFullYear();
+  return namhientai - namsinh;
+}
+const tuoi = tinhtuoi(2005);
+const namsinh = 2005;
+console.log(`năm sinh là ${namsinh}, tuổi là ${tinhtuoi(namsinh)}`);
